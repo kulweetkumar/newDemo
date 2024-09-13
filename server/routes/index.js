c4538edbc061f6b836d98160bdeebd33579d2f36
kanikasharma.cqlsys <kanika.sharma@cqlsys.co.uk>
2024-09-13 11:59:53 +0530

    new code

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const authController = require('../controller/authController')
-
-/* GET home page. */
-
-router.post('/', authController.login)
-
-module.exports = router;
